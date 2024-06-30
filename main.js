fa4d94b36e6dad75d174d75ce38e21ff3415ab41
Akanbi Omolade <omoladegraham@gmail.com>
2024-06-30 19:39:26 +0100

    initial commit

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-const menuBar = document.getElementById("menu-icon");
+const menuBar = document.getElementById("menubar");
+const closeBar = document.getElementById("closeMenu");
 const menuContainer = document.getElementById("mobile_menu_container");
 
 menuBar.addEventListener("click", () => {
   menuContainer.style.display = "flex";
+  menuBar.style.display = "none";
+  closeBar.style.display = "flex";
+});
+
+closeBar.addEventListener("click", () => {
+  menuContainer.style.display = "none";
+  menuBar.style.display = "flex";
+  closeBar.style.display = "none";
 });
